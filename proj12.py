ae216ba0414cdde134d8745cc1147c015d71ad11
VickMASpc <vcmas09@gmail.com>
2025-11-24 07:48:41 -0300

    Delete proj12.py

--- proj12.py
+++
@@ -1,8 +0,0 @@
-def imprimir_numeros_no_intervalo(fim):
-    print(f"Imprimindo números de 0 a {fim-1}:")
-
-    for numero in range(fim):
-        print(numero)
-
-
-imprimir_numeros_no_intervalo(7)
